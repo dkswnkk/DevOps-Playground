97bc7a0d9892777d401a7e632c62dc5e08541510
dkswnkk <jjoy9810@gmail.com>
2022-10-03 15:57:07 +0900

    init: User Service - 프로젝트 생성

---
+++ msa_with_spring_cloud/user-service/src/test/java/com/example/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
