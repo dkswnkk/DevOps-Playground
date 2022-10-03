cb69af250d59e60c06b3fd384ed1adf61978e15e
dkswnkk <jjoy9810@gmail.com>
2022-10-04 01:12:31 +0900

    feat: zuul Conifg 세팅

--- msa_with_spring_cloud/zuul-service/src/main/java/com/example/zuulservice/ZuulServiceApplication.java
+++ msa_with_spring_cloud/zuul-service/src/main/java/com/example/zuulservice/ZuulServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class ZuulServiceApplication {
 
     public static void main(String[] args) {
