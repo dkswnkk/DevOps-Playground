79b3a54f0ca7ddac3b586a578e355eba2963477a
dkswnkk <jjoy9810@gmail.com>
2022-10-04 18:25:27 +0900

    feat: log 출력을 위한 CustomFilter 등록

--- msa_with_spring_cloud/apigateway-service/src/main/java/com/example/apigatewayservice/config/FilterConfig.java
+++ msa_with_spring_cloud/apigateway-service/src/main/java/com/example/apigatewayservice/config/FilterConfig.java
@@ -1,5 +1,6 @@
 package com.example.apigatewayservice.config;
 
+import com.example.apigatewayservice.filter.CustomFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -8,15 +9,17 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class FilterConfig {
     @Bean
-    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder, CustomFilter customFilter) {
         return builder.routes()
                 .route(r -> r.path("/first-service/**")
                         .filters(f -> f.addRequestHeader("first-request", "first-request-header")
-                                .addResponseHeader("first-response", "first-response-header"))
+                                .addResponseHeader("first-response", "first-response-header")
+                                .filter(customFilter.apply(new CustomFilter.Config())))
                         .uri("http://localhost:8081"))
                 .route(r -> r.path("/second-service/**")
                         .filters(f -> f.addRequestHeader("second-request", "second-request-header")
-                                .addResponseHeader("second-response", "second-response-header"))
+                                .addResponseHeader("second-response", "second-response-header")
+                                .filter(customFilter.apply(new CustomFilter.Config())))
                         .uri("http://localhost:8082"))
                 .build();
     }
