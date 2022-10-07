988fe299c520537f898add75d2d0b7c0a6b42d37
dkswnkk <jjoy9810@gmail.com>
2022-10-07 16:01:11 +0900

    feat: Catalogs Microservice - Entity 및 data.sql 생성

---
+++ msa_with_spring_cloud/catalog-service/src/main/java/com/example/catalogservice/entity/CatalogEntity.java
@@ -0,0 +1,21 @@
+package com.example.catalogservice.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "catalog")
+public class CatalogEntity extends BaseTimeEntity implements Serializable {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 120, unique = true)
+    private String productId;
+    @Column(nullable = false)
+    private String productName;
+    @Column(nullable = false)
+    private Integer stock;
+    @Column(nullable = false)
+    private Integer unitPrice;
+
+}
---
+++ msa_with_spring_cloud/catalog-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into catalog(product_id, product_name, stock, unit_price, created_at) values('CATALOG-001', 'Berlin', 100, 1500, now());
+insert into catalog(product_id, product_name, stock, unit_price, created_at) values('CATALOG-002', 'Tokyo', 110, 1100, now());
+insert into catalog(product_id, product_name, stock, unit_price, created_at) values('CATALOG-003', 'Seoul', 120, 1200, now());
